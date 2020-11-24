9dc223d2c1c1ee9a18d3f00694357e9da55785e7
y0584 <yuanm@dtdream.com>
2020-11-24 10:26:43 +0800

    中国地图

--- map/index.js
+++ map/index.js
@@ -1,6 +1,6 @@
 const width = 400;
 const height = 400;
-// 定义svg
+// svg
 const svg = d3.select('body')
     .append('svg')
     .attr('width', width)
@@ -8,18 +8,19 @@ const svg = d3.select('body')
     .attr('transform', `translate(${window.innerWidth / 2 - 200},${window.innerHeight / 2 - 200})`)
     .style('background', 'rgba(0, 0, 0, .4)');
 
-// 设置坐标系
+// 坐标系
 const projection = d3.geoMercator()
     .center([ 93.509174, 42.833888])
     .scale(600)
     .translate([50, 50]);
 
-//  path
+//  path| 生成路径的方法
 const path = d3.geoPath().projection(projection);
 // console.log('path 对象',path);
 
+// 获取json后，画路径呗
 d3.json('../geojson/aa.json').then((data)=>{
-    // console.log('data:',data);
+    
     const groups = svg.append('g')
     groups.selectAll('path')
         .data(data.features)
